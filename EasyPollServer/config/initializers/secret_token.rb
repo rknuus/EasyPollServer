7c3d59cc791bc5ecacad78d1d89e7de9df126df4
Raphael Knaus <r.knuus@gmail.com>
2011-10-20 01:30:29 +0200

    add: rails project skeleton

---
+++ EasyPollServer/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EasyPollServer::Application.config.secret_token = '8184e6117bfdffd58f85b6a84c9cd9562a6dbd50b015491156d6d3153a5697826d2da37d4c81162a4b26366542df64db5442e5323a64d77e294fb1ebbf1cb20b'
