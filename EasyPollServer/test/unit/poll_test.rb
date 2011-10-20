2c57c89571d7500fd81fcaf2bacc90199cc766c5
Raphael Knaus <r.knuus@gmail.com>
2011-10-20 22:17:46 +0200

    add: test invalid if missing attributes

--- EasyPollServer/test/unit/poll_test.rb
+++ EasyPollServer/test/unit/poll_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class PollTest < ActiveSupport::TestCase
-  test "initially published at set" do
+  test "initially published_at set" do
     p = Poll.create
     assert !p.published_at.nil?
+  end
+
+  test "invalid if missing attributes" do
+    p = Poll.create
+    p.published_at = nil
+    assert p.invalid?
+    assert p.errors[:title].any?
+    assert p.errors[:published_at].any?
   end
 end
