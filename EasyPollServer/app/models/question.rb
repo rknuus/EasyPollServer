b1da278d978ffa2a32813d953f8f2af333c99361
Raphael Knaus <r.knuus@gmail.com>
2011-10-26 19:00:42 +0200

    change: merged in changes from A.3 prototypes

--- EasyPollServer/app/models/question.rb
+++ EasyPollServer/app/models/question.rb
@@ -2,4 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :poll
 
   KINDS = ['Yes/No', 'Rate 1..10']
+  
+  validates :text, :kind, :presence => true
+  validates :kind, :inclusion => KINDS
 end
