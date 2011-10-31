9afea06b58458a9196b60abb938ed6d17f4f9531
Raphael Knaus <rknuus@gmail.com>
2011-10-31 23:02:42 +0100

    add: valid question test

--- EasyPollServer/test/unit/question_test.rb
+++ EasyPollServer/test/unit/question_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  test "invalid if missing attributes" do
+  test "should be invalid if missing attributes" do
     question = Question.new
     assert question.invalid?
     assert question.errors[:text].any?
     assert question.errors[:kind].any?
   end
   
-  test "whitelist kind" do
+  test "should whitelist kind" do
     question = Question.new(:text => 'why?', :kind => 'Absolutely Positively Surely Not Existing Kind')
     assert question.invalid?
     assert question.errors[:kind].any?
+  end
+  
+  test "should be valid" do
+    question = Question.new(:text => 'why not?', :kind => Question::KINDS.first)
+    assert question.valid?
   end
 end
