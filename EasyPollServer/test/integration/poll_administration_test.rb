483af05319f51ee1acdeea3989991b0e108769f2
Raphael Knaus <rknuus@gmail.com>
2011-11-01 14:03:01 +0100

    add: test list for integration test

---
+++ EasyPollServer/test/integration/poll_administration_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class PollAdministrationTest < ActionController::TestCase
+  #FIXME: test story poll creation and closing
+  # Alice gets a list of all open polls
+  # Alice logs in
+  # Alice gets a list of all open polls and all polls created by Alice
+  # Alice creates a new poll with 3 questions
+  # Alice closes a poll
+  
+  #FIXME: test story poll participation
+  # Alice gets a list of all open polls
+  # Alice selects a poll
+  # Alice clicks 'participate'
+  # Alice is asked to log in and logs in
+  # Alice fills in the poll questions and sends the answers
+end
