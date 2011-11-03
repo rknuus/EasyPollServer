99873280d750f4d865d69be83d475bdb7ff0b68d
Raphael Knaus <rknuus@gmail.com>
2011-11-03 20:34:02 +0100

    refactor: grouped spec tests

--- spec/requests/poll_administrations_spec.rb
+++ spec/requests/poll_administrations_spec.rb
@@ -24,73 +24,77 @@ module TestHelper #FIXME: merge with unit test_helper.rb
 end
 
 describe "Poll administration" do
-  it "should initially have no active and closed polls" do
-    visit polls_path
-    page.should have_content('Found no active polls.')
-    page.should have_content('Found no closed polls.')
+  describe "GET / polls" do
+    it "should initially have no active and closed polls" do
+      visit polls_path
+      page.should have_content('Found no active polls.')
+      page.should have_content('Found no closed polls.')
+    end
+  
+    it "should open create poll page" do
+      visit polls_path
+      click_link 'Create poll'
+      page.should have_content('New question')
+    end
+  
+    it "should have one active and one closed poll" do
+      TestHelper.create_and_save_poll
+      TestHelper.create_close_and_save_poll
+      visit polls_path
+      page.should_not have_content('Found no active polls.')
+      page.should_not have_content('Found no closed polls.')
+    end
+  
+    #FIXME: how to test Cancel close?
+    it "should close poll" do
+      TestHelper.create_and_save_poll
+      visit polls_path
+      click_button 'Close poll'
+      page.should have_content('Found no active polls.')
+      page.should_not have_content('Found no closed polls.')
+    end
+  
+    #FIXME: how to test Cancel delete?
+    it "should delete poll" do
+      TestHelper.create_close_and_save_poll
+      visit polls_path
+      click_button 'Delete poll'
+      page.should have_content('Found no closed polls.')
+    end
   end
   
-  it "should open create poll page" do
-    visit polls_path
-    click_link 'Create poll'
-    page.should have_content('New question')
-  end
+  describe "GET/PUT/POST new poll" do
+    it "should have only a single question at first" do
+      visit new_poll_path
+      page.should have_content('Question 1')
+      page.should_not have_content('Question 2')
+    end
   
-  it "should have one active and one closed poll" do
-    TestHelper.create_and_save_poll
-    TestHelper.create_close_and_save_poll
-    visit polls_path
-    page.should_not have_content('Found no active polls.')
-    page.should_not have_content('Found no closed polls.')
-  end
+    it "should fail to add new question and not append question when empty" do
+      visit new_poll_path
+      click_button 'Add question'
+      page.should have_content("Text can't be blank")
+      page.should have_content('Question 1')
+      page.should have_xpath("//span[@class='field_with_errors']")
+      page.should_not have_content('Question 2')
+    end
   
-  #FIXME: how to test Cancel close?
-  it "should close poll" do
-    TestHelper.create_and_save_poll
-    visit polls_path
-    click_button 'Close poll'
-    page.should have_content('Found no active polls.')
-    page.should_not have_content('Found no closed polls.')
-  end
-  
-  #FIXME: how to test Cancel delete?
-  it "should delete poll" do
-    TestHelper.create_close_and_save_poll
-    visit polls_path
-    click_button 'Delete poll'
-    page.should have_content('Found no closed polls.')
-  end
-  
-  it "should have only a single question at first" do
-    visit new_poll_path
-    page.should have_content('Question 1')
-    page.should_not have_content('Question 2')
-  end
-  
-  it "should fail to add new question and not append question when empty" do
-    visit new_poll_path
-    click_button 'Add question'
-    page.should have_content("Text can't be blank")
-    page.should have_content('Question 1')
-    page.should have_xpath("//span[@class='field_with_errors']")
-    page.should_not have_content('Question 2')
-  end
-  
-  it "should add one question" do
-    visit new_poll_path
-    fill_in 'poll_questions_attributes_0_text', :with => 'A question'
-    select Question::KINDS.last, :from => 'poll_questions_attributes_0_kind'
-    click_button 'Add question'
-    page.should have_content('Question 2')
-  end
+    it "should add one question" do
+      visit new_poll_path
+      fill_in 'poll_questions_attributes_0_text', :with => 'A question'
+      select Question::KINDS.last, :from => 'poll_questions_attributes_0_kind'
+      click_button 'Add question'
+      page.should have_content('Question 2')
+    end
 
-  it "should fail when publishing without title and category" do
-    visit new_poll_path
-    click_button 'Publish'
-    page.should have_content("Title can't be blank")
-    page.should have_content("Category can't be blank")
-    page.should have_xpath("//div[@class='field_with_errors']")
-  end  
+    it "should fail when publishing without title and category" do
+      visit new_poll_path
+      click_button 'Publish'
+      page.should have_content("Title can't be blank")
+      page.should have_content("Category can't be blank")
+      page.should have_xpath("//div[@class='field_with_errors']")
+    end
+  end
 end
 
 
