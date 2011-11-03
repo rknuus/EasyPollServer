cb1db10963670f040c11e4cef1ad4454acbf4e20
Raphael Knaus <rknuus@gmail.com>
2011-11-03 20:19:38 +0100

    add: specs tests for polls page

--- spec/requests/poll_administrations_spec.rb
+++ spec/requests/poll_administrations_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 
 describe "Poll administration" do
+  it "should initially have no active and closed polls" do
+    visit polls_path
+    page.should have_content('Found no active polls.')
+    page.should have_content('Found no closed polls.')
+  end
+  
+  it "should open create poll page" do
+    visit polls_path
+    click_link 'Create poll'
+    page.should have_content('New question')
+  end
+  
   it "should have only a single question at first" do
     visit new_poll_path
     page.should have_content('Question 1')
     page.should_not have_content('Question 2')
   end
   
-  it "should fail to add new question when empty" do
+  it "should fail to add new question and not append question when empty" do
     visit new_poll_path
     click_button 'Add question'
     page.should have_content("Text can't be blank")
@@ -23,6 +35,14 @@ describe "Poll administration" do
     click_button 'Add question'
     page.should have_content('Question 2')
   end
+
+  it "should fail when publishing without title and category" do
+    visit new_poll_path
+    click_button 'Publish'
+    page.should have_content("Title can't be blank")
+    page.should have_content("Category can't be blank")
+    page.should have_xpath("//div[@class='field_with_errors']")
+  end  
 end
 
 
