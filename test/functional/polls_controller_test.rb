595c6ae2b8a0fa0f8d5b2ea95454dd35d832ed07
Raphael Knaus <rknuus@gmail.com>
2011-11-05 16:52:43 +0100

    change: removed old controller tests which are covered by RSpec tests now
    add: verify inaccessible routes

--- test/functional/polls_controller_test.rb
+++ test/functional/polls_controller_test.rb
@@ -1,56 +1,22 @@
 require 'test_helper'
 
 class PollsControllerTest < ActionController::TestCase
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:active_polls)
-    assert_not_nil assigns(:closed_polls)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  # test "should create poll" do
-  #   poll = new_valid_poll
-  #   assert_difference('Poll.count') do
-  #     get :new
-  #     post :create, poll: poll.attributes
-  #   end
-  # 
-  #   assert_redirected_to poll_path(assigns(:poll))
-  # end
-
-  # test "should show poll" do
-  #   get :show, id: create_valid_poll.to_param
-  #   assert_response :success
-  # end
-
-  # test "should destroy poll" do
-  #   poll = create_valid_poll
-  #   assert_difference('Poll.count', -1) do
-  #     delete :destroy, id: poll.to_param
-  #   end
-  # 
-  #   assert_redirected_to polls_path
-  # end
-  
-  # test "should close poll" do
-  #   poll = create_valid_poll
-  #   put :close, id: poll.to_param, poll: poll.attributes
-  # 
-  #   assert_redirected_to polls_path
-  # end
-  
-  test "should cancel poll" do
-    poll = create_valid_poll
-    assert_no_difference('Poll.count') do
-      get :new
-      post :create, id: poll.to_param, :cancel_button => 'Cancel'
+  test "should not have access to show" do
+    assert_raise(ActionController::RoutingError) do
+      get :show, id: create_valid_poll.to_param
     end
-  
-    assert_redirected_to polls_path
+  end
+
+  test "should not have access to edit" do
+    assert_raise(ActionController::RoutingError) do
+      get :edit, id: create_valid_poll.to_param
+    end
+  end
+
+  test "should not have access to update" do
+    assert_raise(ActionController::RoutingError) do
+      poll = create_valid_poll
+      put :update, id: poll.to_param, poll: poll
+    end
   end
 end
