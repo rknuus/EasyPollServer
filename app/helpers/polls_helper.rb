4b87c1703e3a9aed0c7d083e5217e664fe5c2722
Raphael Knaus <rknuus@gmail.com>
2011-11-06 17:16:06 +0100

    change: tried to replace self written add/delete question by railscast

--- app/helpers/polls_helper.rb
+++ app/helpers/polls_helper.rb
@@ -1,9 +1,16 @@
 module PollsHelper
+  #FIXME: remove?
   def get_question_field_class(symbol)
     if @question_index > @poll.questions.count && @question.errors[symbol].any?
       'field_with_errors'
     else
       'field'
+    end
+  end
+  
+  def add_question_button(name)
+    button_to_function(name) do |page|
+      page.insert_html :bottom, :questions, :partial => 'question', :object => Question.new
     end
   end
 end
