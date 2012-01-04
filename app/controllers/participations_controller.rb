708f2fcababe473d8219441cbb36cfbed68757bb
Ruben Knaus <ruben.knaus@gmail.com>
2012-01-04 22:31:47 +0100

    Delete: Removed unnecessary functions from participations controller

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,21 +1,4 @@
 class ParticipationsController < ApplicationController
-  # GET /participations
-  def index
-    @participations = Participation.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-    end
-  end
-
-  # GET /participations/1
-  def show
-    @participation = Participation.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-    end
-  end
 
   # GET /participations/new
   def new
@@ -26,11 +9,6 @@ class ParticipationsController < ApplicationController
     respond_to do |format|
       format.html # new.html.erb
     end
-  end
-
-  # GET /participations/1/edit
-  def edit
-    @participation = Participation.find(params[:id])
   end
 
   # POST /participations
@@ -56,34 +34,7 @@ class ParticipationsController < ApplicationController
     end
   end
 
-  # PUT /participations/1
-  def update
-    @participation = Participation.find(params[:id])
-
-    respond_to do |format|
-      if @participation.update_attributes(params[:participation])
-        format.html { redirect_to @participation, notice: 'Participation was successfully updated.' }
-      else
-        format.html { render action: "edit" }
-      end
-    end
-  end
-
-  # DELETE /participations/1
-  def destroy
-    @participation = Participation.find(params[:id])
-    @participation.destroy
-
-    respond_to do |format|
-      format.html { redirect_to participations_url }
-    end
-  end
-
 private  
-  def was_button_pressed?(button)
-    params[button]
-  end
-  
   def answered_option_ids
     option_ids = []
     params[:participation][:poll][:questions_attributes].each do |question|
