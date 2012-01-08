93077a0c4dd808862dfa7a1451314603b969c2c8
Ruben Knaus <ruben.knaus@gmail.com>
2012-01-08 17:04:02 +0100

    Fix: Bugfix if participation has no answer for a question - no nice error message yet, form fields are reseted

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -17,31 +17,58 @@ class ParticipationsController < ApplicationController
     @participation.user = User.find(params[:participation][:poll][:user_id])
     @participation.poll = Poll.find(params[:participation][:poll][:poll_id])
     
-    answered_option_ids.each do |option_id|
-      @participation.answers.build(:option_id => option_id)
-    end
+    if answered_option_ids.nil?
+      respond_to do |format|
+        @poll = Poll.find(params[:participation][:poll][:poll_id])
+        format.html { render action: "new" }
+      end
+    else
+      answered_option_ids.each do |option_id|
+        @participation.answers.build(:option_id => option_id)
+      end
 
-    respond_to do |format|
-      if Participation.find(:first, :conditions => "poll_id IS #{@participation.poll.id} AND user_id IS #{@participation.user.id}").nil?
-        if @participation.save
-          format.html { redirect_to "/", notice: 'Participation was successfully created.' }
+      respond_to do |format|
+        if Participation.find(:first, :conditions => "poll_id IS #{@participation.poll.id} AND user_id IS #{@participation.user.id}").nil?
+          if @participation.save
+            format.html { redirect_to "/", notice: 'Participation was successfully created.' }
+          else
+            format.html { render action: "new" }
+          end
         else
-          format.html { render action: "new" }
+          format.html { redirect_to "/", notice: 'You already participated - not saved.' }
         end
-      else
-        format.html { redirect_to "/", notice: 'You already participated - not saved.' }
       end
     end
+    
   end
 
-private  
+private
+
   def answered_option_ids
+    answer_validity = true
     option_ids = []
-    params[:participation][:poll][:questions_attributes].each do |question|
-      params[question[1][:id]].each do |selection|
-        option_ids << selection
+    questions = params[:participation][:poll][:questions_attributes]
+    if !questions.nil? && questions.size > 0
+      questions.each do |question|
+        selections = params[question[1][:id]]
+        
+        if !selections.nil? && selections.size > 0
+          selections.each do |selection|
+            option_ids << selection
+          end
+        else
+          answer_validity = false
+        end
+        
       end
+    else
+      answer_validity = false
     end
-    option_ids
+    
+    if answer_validity
+      option_ids
+    else
+      nil
+    end
   end
 end
